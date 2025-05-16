cb3c5771b87132e9bcbf45c174255697b9b451fc
Daniel Vinicius <131543313+DanielVinicius07@users.noreply.github.com>
2025-05-16 15:12:16 -0300

    commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/'), (req,res =>{
+    res.send('Olá, Mundo');
+});
+
+app.get('/Home'), (req,res =>{
+    res.send('#home');
+});
+
+app.get('/about'), (req,res =>{
+    res.send('#about');
+});
+
+app.get('/features'), (req,res =>{
+    res.send('#features');
+});
+
+app.get('/contact'), (req,res =>{
+    res.send('#contact');
+});
